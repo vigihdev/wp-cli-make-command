5ef755a77b6077c5d85318fb46e0bd113db7fa28
vigih dev <vigihdev@gmail.com>
2025-12-04 14:43:30 +0700

    💥 4-12-2025 14-35

---
+++ make-command.php
@@ -0,0 +1,15 @@
+<?php
+
+use Vigihdev\WpCliMake\Commands\{Menu_Make_Command, Post_Make_Command};
+
+if (! class_exists('WP_CLI')) {
+    return;
+}
+
+$autoloader = __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoloader)) {
+    require_once $autoloader;
+}
+
+WP_CLI::add_command('make:post', new Post_Make_Command());
+WP_CLI::add_command('make:menu', new Menu_Make_Command());
